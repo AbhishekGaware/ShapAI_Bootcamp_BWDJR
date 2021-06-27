5b68d273abea2076a97adf8ce88b20d8708142a8
AbhishekGaware <71840135+AbhishekGaware@users.noreply.github.com>
2021-06-27 12:03:46 +0530

    Add files via upload

---
+++ info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and ReacJS</h1>
+      <p>Basic of JavaScript and React</p>
+    </div>
+  );
+}
+
+export default Info;
